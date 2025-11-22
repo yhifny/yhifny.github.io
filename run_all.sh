b5e0a5e1963265ef7e83050f8987ab0e9019f429
Yasser Hifny <yhifny@yahoo.com>
2025-11-22 11:38:07 +0200

    new version of the book

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+# create a new repo locally
+
+#git init
+# copy index.html into this folder (or save it directly here)
+#git add index.html
+#git commit -m "Initial site"
+# create repo on GitHub (you can also create it via the GitHub website)
+#git remote add origin https://github.com/yhifny/yhifny.github.io.git
+git push -u origin master
